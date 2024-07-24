4bd0be9083738b05ec361c9dac8901f5c9dea189
linnabraham <linn.official@gmail.com>
2024-07-24 18:05:34 +0530

    Add output of latex2html

---
+++ solar/internals.pl
@@ -0,0 +1,38 @@
+# LaTeX2HTML 2024 (Released January 1, 2024)
+# Associate internals original text with physical files.
+
+
+$key = q/cite_choudhuri_natures_2015/;
+$ref_files{$key} = "$dir".q|node13.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_priest_magnetohydrodynamics_nodate/;
+$ref_files{$key} = "$dir".q|node13.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_shu_physical_1982/;
+$ref_files{$key} = "$dir".q|node13.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:AIA/;
+$ref_files{$key} = "$dir".q|node12.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:corona/;
+$ref_files{$key} = "$dir".q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:flare/;
+$ref_files{$key} = "$dir".q|node10.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:magnetogram/;
+$ref_files{$key} = "$dir".q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:sunspot/;
+$ref_files{$key} = "$dir".q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
